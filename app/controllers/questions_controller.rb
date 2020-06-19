4fdede31d722de2566adf1b21e3156637c733218
Timur Pavlov <brillertv@gmail.com>
2020-06-19 23:08:04 +0300

    lesson_8

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_question, only: %i[show destroy edit update]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-  end
-
-  def new
+  def show
   end
 
   def create
@@ -18,16 +16,28 @@ class QuestionsController < ApplicationController
     end
   end
 
-  def show
-    @question = Question.find(params[:id])
+  def new
+    @question = @test.questions.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to test_path(@question.test)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy
-    redirect_to action: 'index', id: params[:test_id]
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
+
   def find_test
     @test = Test.find(params[:test_id])
   end
