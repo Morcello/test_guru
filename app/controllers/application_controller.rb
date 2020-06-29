71894b627365b0f6c37023a561e5ebe57927a656
Timur Pavlov <brillertv@gmail.com>
2020-06-29 19:17:21 +0300

    fix_2 lesson_10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
-      cookies[:path] = request.fullpath
+      cookies[:part] = request.fullpath
       return redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password, please.'
     end
 
